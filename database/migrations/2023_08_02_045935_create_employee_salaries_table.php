c8e6e6c64850d6a9df34c2b1892229b332ac80ea
sangeetha santhiralingam <sangeethasanthiralingam>
2023-08-02 13:46:19 +0530

    m

--- database/migrations/2023_08_02_045935_create_employee_salaries_table.php
+++ database/migrations/2023_08_02_045935_create_employee_salaries_table.php
@@ -17,16 +17,21 @@ return new class extends Migration
             $table->id();
             $table->string('bio_code');
             $table->foreign('bio_code')->references('bio_code')->on('employees')->onDelete('cascade');
-            $table->biginteger('employee_id');
-            $table->foreign('employee_id')->references('id')->on('employees')->onDelete('setnull');
-            $table->decimel('epf');
-            $table->decimel('etf');
+            $table->bigInteger('employee_id')->unsigned()->nullable();
+            $table->foreign('employee_id')->references('id')->on('employees')->onDelete('cascade');
+            $table->decimal('epf', 10, 2); // The 'decimal' method takes precision and scale as arguments.
+            $table->decimal('etf', 10, 2); // Here, I assumed a precision of 10 and scale of 2. You can adjust as needed.
             $table->string('status');
-            $table->decimel(salary_amount);
+            $table->decimal('salary_amount', 10, 2);
+            $table->unsignedBigInteger('added_by');
+            $table->foreign('added_by')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('approved_by');
+            $table->foreign('approved_by')->references('id')->on('users')->onDelete('cascade');
             $table->date('date');
-            $table->timestamps();//ww
+            $table->timestamps();
         });
     }
+    
 
     /**
      * Reverse the migrations.
