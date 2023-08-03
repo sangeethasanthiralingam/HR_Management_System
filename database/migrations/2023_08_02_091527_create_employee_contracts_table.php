0dd087acd5967c0a440ee7b58d2785bdfebda636
sangeetha santhiralingam <sangeethasanthiralingam>
2023-08-03 09:51:35 +0530

    remove id from em

--- database/migrations/2023_08_02_091527_create_employee_contracts_table.php
+++ database/migrations/2023_08_02_091527_create_employee_contracts_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('employee_contracts', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('employee_id')->unsigned()->nullable();
-            $table->foreign('employee_id')->references('id')->on('employees')->onDelete('set null');
+            $table->bigInteger('employee')->unsigned()->nullable();
+            $table->foreign('employee')->references('id')->on('employees')->onDelete('set null');
             $table->date('contract_start_date');
             $table->date('contract_end_date');
             $table->string('job_description');
